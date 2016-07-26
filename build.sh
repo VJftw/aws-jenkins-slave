d377a62567f7a1556d8db0adbe097a5eb2376bde
VJ Patel <meetthevj@gmail.com>
2016-07-27 00:25:54 +0100

    forgot sudo!

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ sudo apt-get install -y apt-transport-https ca-certificates
 echo "--> Setting up Docker repository"
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 if [ -f /etc/apt/sources.list.d/docker.list ]; then
-    rm /etc/apt/sources.list.d/docker.list
+    sudo rm /etc/apt/sources.list.d/docker.list
 fi
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" | sudo tee -a /etc/apt/sources.list.d/docker.list
 
