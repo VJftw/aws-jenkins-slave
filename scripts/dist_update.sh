8587405f7bb2a566ba50ca7541364749b29e6abe
VJ Patel <meetthevj@gmail.com>
2016-10-23 00:34:42 +0100

    Update dist_update.sh

--- scripts/dist_update.sh
+++ scripts/dist_update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export DEBIAN_FRONTEND="noninteractive"
-sudo echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 
 sudo apt-get update -y
 sudo apt-get dist-upgrade -y
