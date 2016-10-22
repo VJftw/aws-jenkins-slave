c9d1d60cba4feb608b38305eb1115ca3415df429
VJ Patel <meetthevj@gmail.com>
2016-10-23 00:32:16 +0100

    Update pre_install.sh

--- scripts/pre_install.sh
+++ scripts/pre_install.sh
@@ -6,8 +6,8 @@ echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 sudo apt-get update -y
 sudo apt-get install -y python3 python3-dev curl apt-transport-https ca-certificates
 
-cd /tmp
 curl -O -L https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
+rm get-pip.py*
 
 sudo pip3 install --upgrade -r requirements.txt
