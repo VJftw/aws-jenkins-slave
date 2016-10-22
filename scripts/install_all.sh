7a59a4ec4dfcf4305177d6879114a5c98c9851af
VJ Patel <meetthevj@gmail.com>
2016-10-23 00:34:30 +0100

    Update install_all.sh

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND="noninteractive"
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
+echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 
 # Docker PPA
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
