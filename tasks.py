06056d7bd6c49ace4bc30c14f480cf705248be44
VJ Patel <meetthevj@gmail.com>
2016-10-23 02:27:17 +0100

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -7,14 +7,16 @@ import boto3
 @task
 def create_ami(ctx):
     client = boto3.client('ec2', region_name='eu-west-1')
-    instance_id = urllib.request.urlopen("http://169.254.169.254/latest/meta-data/instance-id").read()
+    instance_id = urllib.request.urlopen("http://169.254.169.254/latest/meta-data/instance-id").read().decode("utf-8")
     ami_name = "jenkins-slave_{0}".format(time.strftime("%Y%m%d-%H%M"))
 
     response = client.create_image(
         Name=ami_name,
-        InstanceId=instance_id.decode("utf-8"),
+        InstanceId=instance_id,
         NoReboot=True
     )
+    
+    print("\nCreating {0}...".format(instance_id))
 
     waiter = client.get_waiter('image_available')
     waiter.wait(
@@ -22,4 +24,5 @@ def create_ami(ctx):
             response['ImageId']
         ]
     )
+    print("\nCreated {0}.".format(instance_id))
 
